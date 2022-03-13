df7feff4acc424a8a427d9a7b91d6723b09caea5
sara20200540 <sara.20200540@iit.ac.lk>
2022-03-13 19:50:09 +0530

    Load model - works

--- image_classifying_api.py
+++ image_classifying_api.py
@@ -11,19 +11,41 @@
 
 #!/usr/bin/env python
 # encoding: utf-8
-import json
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def query_records():
-    name = request.args.get('name')
-    print(name)
+from json import load
+from numpy import loadtxt
+from keras.models import load_model
+from keras.preprocessing import image
+
+# load model
+model = load_model('./2 Classes 1 Epoch Model')
+
+# summarize model.
+model.summary()
+# load dataset
+# dataset = loadtxt("pima-indians-diabetes.csv", delimiter=",")
+
+image = image.load_img('erythema-multiforme-88.jpg', target_size=(350, 750))
+image.show()
+
+# split into input (X) and output (Y) variables
+# X = dataset[:,0:8]
+# Y = dataset[:,8]
+# evaluate the model
+# score = model.evaluate(X, Y, verbose=0)
+# print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
+
+# app = Flask(__name__)
+
+
+# @app.route('/', methods=['GET'])
+# def query_records():
+#     name = request.args.get('name')
+#     print(name)
     # Read image and classify
-
-    return ('Eczema')
+    # scores = model.evaluate(testImage)
+    # return ('Eczema')
 
     # return jsonify({'disease': 'Eczema'})
 
@@ -75,4 +97,4 @@ def query_records():
 #     return jsonify(record)
 
 
-app.run(debug=True)
+# app.run(debug=True)
