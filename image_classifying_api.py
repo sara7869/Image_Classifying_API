09deb2fe2737d6643bf4b52fbcad91836e60526e
sara20200540 <sara.20200540@iit.ac.lk>
2022-03-22 13:04:43 +0530

    Implemented saving image received

--- image_classifying_api.py
+++ image_classifying_api.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import pathlib
+import os
 from flask import Flask
 
 # !/usr/bin/env python
@@ -11,6 +13,8 @@ from json import load
 from numpy import loadtxt
 from keras.models import load_model
 from keras.preprocessing import image
+from flask import request
+from werkzeug.utils import secure_filename
 
 # Source: https://www.geeksforgeeks.org/how-to-convert-images-to-numpy-array/
 import PIL
@@ -27,33 +31,17 @@ model.summary()
 # dataset = loadtxt("pima-indians-diabetes.csv", delimiter=",")
 
 # image = image.load_img('erythema-multiforme-88.jpg', target_size=(128, 128))
-image = image.load_img('03ichthyosis0213061.jpg', target_size=(128, 128))
+# image = image.load_img('03ichthyosis0213061.jpg', target_size=(128, 128))
 # image = image.load_img('03ichthyosis0213061.jpg', target_size=(48, 48, 1))
 
 
 # image = np.expand_dims(image, axis=3)
-image = np.expand_dims(image, axis=0)
+# image = np.expand_dims(image, axis=0)
 # image = image.resize((128,128), Image.ANTIALIAS)
 
 # image.show()
 # image = Image.open('erythema-multiforme-88.jpg', target_s)
-numpydata = asarray(image)
 
-print(numpydata.shape)
-
-predictions = model.predict(numpydata, batch_size=None, verbose=1, steps=None, callbacks=None,
-                            max_queue_size=10, workers=1, use_multiprocessing=False)
-
-class_names = ['Acne and Rosacea Photos',
-               'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions',
-               'Atopic Dermatitis Photos',
-               'Bullous Disease Photos',
-               'Cellulitis Impetigo and other Bacterial Infections']
-
-predicted_class = class_names[np.argmax(predictions[0])]
-
-print(predictions[0])
-print(predicted_class)
 
 # split into input (X) and output (Y) variables
 # X = dataset[:,0:8]
@@ -64,6 +52,28 @@ print(predicted_class)
 
 app = Flask(__name__)
 
+# Assigns upload path to variable
+folder = os.path.join(app.instance_path, 'uploads')
+
+def classifyImage():
+    numpydata = asarray(image)
+
+    print(numpydata.shape)
+
+    predictions = model.predict(numpydata, batch_size=None, verbose=1, steps=None, callbacks=None,
+                                max_queue_size=10, workers=1, use_multiprocessing=False)
+
+    class_names = ['Acne and Rosacea Photos',
+                   'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions',
+                   'Atopic Dermatitis Photos',
+                   'Bullous Disease Photos',
+                   'Cellulitis Impetigo and other Bacterial Infections']
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+
+    print(predictions[0])
+    print(predicted_class)
+
 
 @app.route('/', methods=['POST'])
 def query_records():
@@ -72,6 +82,22 @@ def query_records():
     # Read image and classify
     # scores = model.evaluate(testImage)
     # return ('Eczema')
+
+    files = request.files
+    file = files.getlist('file_contents')[0]
+    filename = file.filename
+    filename = secure_filename(filename)
+
+    current_directory_path = pathlib.Path(__file__).parent.resolve()
+    current_directory_path_string = current_directory_path.__str__()
+    path_at_uploads = current_directory_path_string + '/uploads/'
+    path = os.path.join(path_at_uploads, secure_filename(filename))
+    file.save(path)
+
+    print(request.files)
+    classifyImage()
+    return json.dumps(file)
+
     return json.dumps({'disease': predicted_class})
 
     # return jsonify({'disease': 'Eczema'})
